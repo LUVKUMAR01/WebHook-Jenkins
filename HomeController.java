da7e679dce7c901e2eaefe62872bd334b928ca13
luvkumar01 <luvsahu24@gmail.com>
2025-11-20 13:04:51 +0530

    Webhook Jenkins

---
+++ HomeController.java
@@ -0,0 +1,15 @@
+package com.example.Webhook_Jenkins;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/hello")
+	public String hellotest() {
+		return "This is Hello World!!";
+		
+	}
+
+}
---
+++ WebhookJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Webhook_Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookJenkinsApplication.class, args);
+	}
+
+}
